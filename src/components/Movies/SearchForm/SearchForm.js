537992eb1238e261ea53064d4bf83d980ada5c80
ale-lit <zeldor@ya.ru>
2022-04-29 23:53:43 +0300

    feat: add adaptive (768) for films

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ function SearchForm() {
     return (
         <section className="search project__wrapper">
             <div className="search__form">
-                <input type="search" className="search__input" name="" id="" placeholder="Фильм" />
+                <input type="search" className="search__input" placeholder="Фильм" />
                 <input type="button" className="search__button" aria-label="Найти" />
             </div>
             <FilterCheckbox />
