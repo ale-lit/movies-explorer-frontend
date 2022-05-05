375c4c6330854694431a4963b9dd0764cf32e68d
ale-lit <zeldor@ya.ru>
2022-05-05 19:54:33 +0300

    fix: add required from search form

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ function SearchForm() {
     return (
         <section className="search project__wrapper">
             <div className="search__form">
-                <input type="search" className="search__input" placeholder="Фильм" />
+                <input type="search" className="search__input" placeholder="Фильм" required />
                 <input type="button" className="search__button" aria-label="Найти" />
             </div>
             <FilterCheckbox />
