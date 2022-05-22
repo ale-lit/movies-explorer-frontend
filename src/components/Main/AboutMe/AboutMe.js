8b1d94e3d9abe72a05f8171e5c0460b8a58994b5
ale-lit <zeldor@ya.ru>
2022-05-22 15:05:19 +0300

    Update AboutMe.js

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -14,13 +14,19 @@ function AboutMe() {
                         Фронтенд-разработчик, 36 лет
                     </p>
                     <p className="student__description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Recusandae tenetur illum et nobis accusamus dignissimos
-                        ab corrupti assumenda adipisci numquam quod autem, ullam
-                        aut veniam natus tempora atque dolore modi! Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Lorem ipsum
-                        dolor sit amet...
+                        Я родился и живу в Санкт-Петербурге, Веб-разработкой
+                        увлекаюсь с 2006 года (в основном как хобби в свободное
+                        от работы время, но также периодически выполнял
+                        некоторые фриланс-заказы). Имею высшее экономическое
+                        образование по направлению «Финансы и кредит». В
+                        середине 2021 года решил что-то кардинально поменять в
+                        своей жизни, начав профессионально заниматься тем что
+                        мне действительно очень нравится, и поступил на курс
+                        «Фронтенд-разработчик» в Яндекс.Практикуме (который
+                        закончил, успешно защитив диплом в мае 2022 года), а
+                        также на курс профессиональной переподготовки в СПбПУ по
+                        направлению «Веб-разработчик. Язык PHP» (заканчиваю в
+                        августе 2022г).
                     </p>
                     <ul className="student__links">
                         <li className="student__link">
@@ -52,7 +58,7 @@ function AboutMe() {
                         className="student__photo-card"
                     />
                 </div>
-            </div>            
+            </div>
         </section>
     );
 }
